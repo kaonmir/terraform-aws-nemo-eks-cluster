bcbed636b75961ea8e2fd7b93431562d055549cc
kaonmir <thswpvm@cau.ac.kr>
2022-06-10 21:29:41 +0900

    space of json fuck

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -2,18 +2,18 @@ resource "aws_iam_role" "eks_cluster" {
   name = "${var.cluster_name}-eks_cluster"
 
   assume_role_policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "eks.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
   POLICY
 }
 
